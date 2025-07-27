2c76055f7198aa05bbb449adfeb1983c9227c6c2
Maieul BOYER <maieul.dev@familleboyer.net>
2025-07-27 15:20:17 +0200

    update: yes

--- config.nix
+++ config.nix
@@ -5,7 +5,19 @@
   lib,
   ...
 }: {
-  config = {
+  config = let
+    tree-sitter-dbml = pkgs.tree-sitter.buildGrammar {
+      language = "dbml";
+      version = "0.0.0+rev=2e2fa56";
+      src = pkgs.fetchFromGitHub {
+        owner = "dynamotn";
+        repo = "tree-sitter-dbml";
+        rev = "2e2fa5640268c33c3d3f27f7e676f631a9c68fd9";
+        hash = "sha256-IxxUW6YYxP1hkwA9NEojEEE3c8pwvAI6juX8aF7NfMw=";
+      };
+      meta.homepage = "https://github.com/dynamotn/tree-sitter-dbml";
+    };
+  in {
     colorschemes.tokyonight = {
       settings.style = "night";
       enable = true;
@@ -427,7 +439,26 @@
         yaml
         mermaid
         fish
+        tree-sitter-dbml
       ];
+
+      luaConfig.post = ''
+        do
+          local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+          -- change the following as needed
+          parser_config.nu = {
+            install_info = {
+              url = "${tree-sitter-dbml}", -- local path or git repo
+              files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+              requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+              -- optional entries:
+              --  branch = "main", -- default branch in case of git repo if different from master
+              -- generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+            },
+            filetype = "dbml", -- if filetype does not match the parser name
+          }
+        end
+      '';
 
       nixvimInjections = true;
     };
@@ -811,6 +842,7 @@
       vim-snippets
       markdown-preview-nvim
       ft-std-header
+      tree-sitter-dbml
     ];
   };
 }
